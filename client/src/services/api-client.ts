2711fb22314b217b6c9798b74a5d222ae5fbeacc
HEKNOWSME <iranziclaude@hotmail.com>
2025-02-21 16:57:25 +0200

    s

--- client/src/services/api-client.ts
+++ client/src/services/api-client.ts
@@ -1,3 +1,3 @@
 import axios, {CanceledError} from "axios";
-export default axios.create({ baseURL: "http://localhost:3000/api/" })
+export default axios.create({ baseURL: "https://mern-authentication-n611.onrender.com/api/" })
 export {CanceledError}
